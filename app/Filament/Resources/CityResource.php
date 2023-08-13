1840f314d5cd25a02c71f4f1d05ffe2785999128
sayedsaddam <48250591+sayedsaddam@users.noreply.github.com>
2023-08-13 23:04:04 +0500

    chore: created filament resource for adding and displaying cities.

---
+++ app/Filament/Resources/CityResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use App\Models\City;
+use Filament\Tables;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\CityResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\CityResource\RelationManagers;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+
+class CityResource extends Resource
+{
+    protected static ?string $model = City::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()
+                ->schema([
+                    Select::make('state_id')
+                    ->relationship('state', 'name'),
+                    TextInput::make('name')
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('name')->sortable()->searchable(),
+                TextColumn::make('state.name')->sortable()->searchable(),
+                TextColumn::make('created_at')->date()->sortable()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCities::route('/'),
+            'create' => Pages\CreateCity::route('/create'),
+            'edit' => Pages\EditCity::route('/{record}/edit'),
+        ];
+    }
+}
