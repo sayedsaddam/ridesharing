faa65c522882978bdb3c87dfa78522bccd9697a9
sayedsaddam <48250591+sayedsaddam@users.noreply.github.com>
2023-08-14 01:14:36 +0500

    chore: updated 3 files including filament resources and model, added fillable fields into the model, created filament resource for employee addition and retrieval.

---
+++ app/Filament/Resources/EmployeeResource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EmployeeResource\Pages;
+use App\Filament\Resources\EmployeeResource\RelationManagers;
+use App\Models\Employee;
+use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class EmployeeResource extends Resource
+{
+    protected static ?string $model = Employee::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()
+                ->schema([
+                    Select::make('country_id')
+                        ->relationship('country', 'name')->required(),
+                    Select::make('state_id')
+                        ->relationship('state', 'name')->required(),
+                    Select::make('city_id')
+                        ->relationship('city', 'name')->required(),
+                    Select::make('department_id')
+                        ->relationship('department', 'name')->required(),
+                    TextInput::make('first_name')->required(),
+                    TextInput::make('last_name')->required(),
+                    TextInput::make('address')->required(),
+                    TextInput::make('zip_code')->required(),
+                    DatePicker::make('birth_date')->required(),
+                    DatePicker::make('date_hired')->required()
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('first_name')->sortable()->searchable(),
+                TextColumn::make('last_name')->sortable()->searchable(),
+                TextColumn::make('department.name')->sortable(),
+                TextColumn::make('date_hired')->date(),
+                TextColumn::make('created_at')->date()
+            ])
+            ->filters([
+                SelectFilter::make('department')->relationship('department', 'name')
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEmployees::route('/'),
+            'create' => Pages\CreateEmployee::route('/create'),
+            'edit' => Pages\EditEmployee::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/State.php
+++ app/Models/State.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class State extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'country_id',
+        'name',
+    ];
+
+    public function country(){
+        return $this->belongsTo(Country::class);
+    }
+
+    public function employees(){
+        return $this->hasMany(Employee::class);
+    }
 }
