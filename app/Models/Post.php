2b74a0d57ae9bf1783bb4f90024b83728a38a2ee
sayedsaddam <48250591+sayedsaddam@users.noreply.github.com>
2023-08-15 13:05:11 +0500

    chore: updated the models, created relationships with other models, defined the fillable fields inside the models.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['category_id', 'title', 'slug', 'content', 'is_published'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
 }
