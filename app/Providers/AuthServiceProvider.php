30ad08914e998979891463574b9a0da87bdd9440
sayedsaddam <48250591+sayedsaddam@users.noreply.github.com>
2023-08-23 00:21:51 +0500

    chore: updated authserviceprovider to register the policies just created for role and user.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Policies\RolePolicy;
+use App\Policies\UserPolicy;
 use App\Policies\PermissionPolicy;
 use Spatie\Permission\Models\Permission;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -17,6 +19,8 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
         Permission::class => PermissionPolicy::class,
+        Role::class => RolePolicy::class,
+        User::class => UserPolicy::class,
     ];
 
     /**
