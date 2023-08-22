d95e47b5e7c8b635c5221290f9bf170a822b4677
sayedsaddam <48250591+sayedsaddam@users.noreply.github.com>
2023-08-22 11:29:05 +0500

    updated few file, installed spatie laravel permission package, created new migration and seeder to seed the databases with pre seeded roles for user.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,14 @@ namespace App\Models;
 use App\Models\Trip;
 use App\Models\Driver;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
